f4a8ef3058ff1643cdfc9fee7938a57a7959b3b6
rezatajari <reza.tajari70@gmail.com>
2021-02-25 15:56:39 +0330

    employee model and repository of it

---
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeRepository _employeeRepository;
+
+        public EmployeeController(EmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public string Index(int Id)
+        {
+            return _employeeRepository.GetEmployee(Id);
+        }
+    }
+}
---
+++ EmployeeManagement/Models/EmployeeRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeesList;
+
+        public EmployeeRepository()
+        {
+            _employeesList = new List<Employee>
+            {
+                new Employee{ Id=1,Name="Reza",Email="Reza@gmail.com",Department="Stuff"},
+                new Employee{ Id=2,Name="Ali",Email="Ali@gmail.com",Department="Driver"},
+                new Employee{ Id=3,Name="Mohammad",Email="Mohammad@gmail.com",Department="University"},
+                new Employee{ Id=4,Name="Ehsan",Email="Ehsan@gmail.com",Department="Country"}
+            };
+
+        }
+
+        public string GetEmployee(int Id)
+        {
+            return _employeesList.FirstOrDefault(f => f.Id == Id).Name;
+        }
+    }
+}
