b69f60802cec4a216fa6d0a82c179274539f4546
rezatajari <reza.tajari70@gmail.com>
2021-03-25 17:15:47 +0430

    implementation register api

--- EmployeeManagement/Controllers/AccountController.cs
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +11,40 @@ namespace EmployeeManagement.Controllers
     [Route("Account")]
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+                                 SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         [HttpGet, Route("Register")]
         public ViewResult Register()
         {
             return View();
+        }
+
+        [HttpPost, Route("Register")]
+        public async Task<IActionResult> Register(RegisterViewModel registerMode)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser() { UserName = registerMode.Email, Email = registerMode.Email };
+                var result = await _userManager.CreateAsync(user, registerMode.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError("", error.Description);
+            }
+
+            return View(registerMode);
         }
     }
 }
